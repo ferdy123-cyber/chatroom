66106a05e6285e32706d8185bf08cd8584dbcdbb
ferdy <cepat9986@gmail.com>
2021-08-23 18:08:36 +0700

    :)

--- src/components/protectedRouter/PageRouter.js
+++ src/components/protectedRouter/PageRouter.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from "react-router-dom";
 const PageRouter = (props) => {
   if (
     localStorage.getItem("chatLogin") === "false" ||
-    localStorage.getItem("chatLogin")
+    !localStorage.getItem("chatLogin")
   ) {
     return <Redirect to="/login" />;
   } else {
